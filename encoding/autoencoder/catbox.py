deaf66e5be7a8f34a73726734bdc6d428539d3ad
kirelic <108435842+kirelic@users.noreply.github.com>
2022-06-29 17:59:20 -0400

    Update catbox.py

--- encoding/autoencoder/catbox.py
+++ encoding/autoencoder/catbox.py
@@ -3,63 +3,160 @@ import datetime
 import os
 import re
 import sys
+from urllib.parse import urlparse
+import time
+from datetime import timedelta
 
-def upload(file):
-    host = "https://catbox.moe/user/api.php"
-    origname = file
-    if(re.match(r"^.*\.webm$", file)):
-        mime_type = "video/webm"
-        ext = ".webm"
-    elif(re.match(r"^.*\.mp3$", file)):
-        mime_type = "audio/mpeg"
-        ext = ".mp3"
-    else:
-        return None
-    if userhash:
-        payload = {'reqtype': 'fileupload', 'userhash': userhash}
-    else:
-        payload = {'reqtype': 'fileupload'}
-    timestamp = str(int(datetime.datetime.now().timestamp()))
-    file = "temp" + timestamp + ext
-    os.rename(origname, file)  # fixes special character errors
-    f = open(file, 'rb')
-    files = {'fileToUpload': (file, f, mime_type)}
-    response = requests.post(host, data=payload, files=files)
+
+def upload(folder):
+    folderList = []
+    folderName = ""
+    fileList = []
+    uploadedList = []
+    deleteFileList = []
+    deleteFolderList = []
+    file = ''
+    fileToFolder = ''
+    fileToDelete = ''
+    folderToDelete = ''
+    
+    rootdir = os.getcwd() + '\\' + folder
+    for file in os.listdir(rootdir):
+        d = os.path.join(rootdir, file)
+        if os.path.isdir(d):
+            folderList.append(d)
+            
+    currentFolder = 0
+    while currentFolder < len(folderList):
+        fileList = os.listdir(folderList[currentFolder])
+        currentFile = 0
+        while currentFile < len(fileList):
+            file = folderList[currentFolder] + '\\' +fileList[currentFile]
+            host = "https://catbox.moe/user/api.php"
+            origname = file
+            if(re.match(r"^.*\.webm$", file)):
+                mime_type = "video/webm"
+                ext = ".webm"
+            elif(re.match(r"^.*\.mp4$", file)):
+                mime_type = "video/mp4"
+                ext = ".mp4"                   
+            elif(re.match(r"^.*\.mp3$", file)):
+                mime_type = "audio/mpeg"
+                ext = ".mp3"   
+            elif(re.match(r"^.*\.jpg$", file)):
+                mime_type = "image/jpeg"
+                ext = ".jpg"
+            elif(re.match(r"^.*\.jpeg$", file)):
+                mime_type = "image/jpeg"
+                ext = ".jpeg"
+            elif(re.match(r"^.*\.png$", file)):
+                mime_type = "image/png"
+                ext = ".png"
+            elif(re.match(r"^.*\.gif$", file)):
+                mime_type = "image/gif"
+                ext = ".gif"            
+            else:
+                return None
+            if userhash:
+                payload = {'reqtype': 'fileupload', 'userhash': userhash}
+            else:
+                payload = {'reqtype': 'fileupload'}
+            timestamp = str(int(datetime.datetime.now().timestamp()))
+            file = "temp" + timestamp + ext
+            os.rename(origname, file)  # fixes special character errors
+            f = open(file, 'rb')
+            files = {'fileToUpload': (file, f, mime_type)}
+            response = requests.post(host, data=payload, files=files)
+            f.close()
+            os.rename(file, origname)
+            if response.ok:
+                print("picture upload success: %s" % response.text)
+                a = urlparse(response.text)
+                uploadedList.append(os.path.basename(a.path))
+                deleteFileList.append(os.path.basename(a.path))
+                #print (os.path.basename(a.path))
+            else:
+                print("upload failed: %s" % response.text)            
+            currentFile += 1
+
+        fileToFolder = ' '.join(uploadedList)
+        payload = {'reqtype': 'createalbum', 'userhash': userhash, 'title': os.path.basename(folderList[currentFolder]), 'desc': os.path.basename(folderList[currentFolder])+ "'s folder", 'files' : fileToFolder}
+        response = requests.post(host, data=payload)
+        if response.ok:
+            print()
+            print("folder creation success: %s" % response.text)
+            print()
+            a = urlparse(response.text)
+            deleteFolderList.append(os.path.basename(a.path))            
+            f = open("uploaded.txt", "a")
+            f.write(os.path.basename(folderList[currentFolder]) + ' - ' + response.text + '\n')
+            f.close()            
+        else:
+            print("upload failed: %s" % response.text) 
+        uploadedList.clear()
+        currentFolder += 1  
+        
+    
+    fileToDelete = ' '.join(deleteFileList)
+    f = open("deleteFiles.txt", "a")
+    f.write(fileToDelete)
+    f.close()   
+    
+    folderToDelete = ' '.join(deleteFolderList)
+    f = open("deleteFolders.txt", "a")
+    f.write(folderToDelete)
+    f.close()       
+
+
+
+
+def deletePics():
+    f = open("deleteFiles.txt", "r")
+    filesToDelete = f.read()
     f.close()
-    os.rename(file, origname)
-    if response.ok:
-        print("upload success: %s" % response.text)
-        return response.text
-    else:
-        print("upload failed: %s" % response.text)
-        return None
-
-
-def upload_from_url(url):
-    print("mirroring %s to catbox" % url)
+    
     host = "https://catbox.moe/user/api.php"
-    if userhash:
-        payload = {"reqtype": "urlupload", "userhash": userhash, "url": url}
-    else:
-        payload = {"reqtype": "urlupload", "url": url}
+    payload = {'reqtype': 'deletefiles', 'userhash': userhash, 'files' : filesToDelete}
     response = requests.post(host, data=payload)
     if response.ok:
-        print("mirror success: %s" % response.text)
-        try:
-            caturl = response.text
-            source_extension = re.match(r".*\.(\w+)$", url).group(1)
-            cat_extension = re.match(r".*\.(\w+)$", caturl).group(1)
-            if cat_extension != source_extension:
-                f = open("catfail.txt", "a", encoding="utf-8")
-                f.write("%s -> %s\n" % (url, caturl))
-                f.close()
-                print("%s -> %s" % (url, caturl))
-        except Exception:
-            pass
-        return response.text
+        print("deletion was a success: %s" % response.text)         
     else:
-        print("mirror failed: %s" % response.text)
-        return None
+        print("delete failed: %s" % response.text)  
+        
+
+def deleteAlbum():
+    f = open("deleteFolders.txt", "r")
+    foldersToDelete = f.read().split(" ")
+    f.close()    
+    
+    host = "https://catbox.moe/user/api.php"
+    
+    x = 0
+    while x < len(foldersToDelete): 
+        payload = {'reqtype': 'deletealbum', 'userhash': userhash, 'short' : foldersToDelete[x]}
+        response = requests.post(host, data=payload)
+        if response.ok:
+            print("Album was deleted successfully: %s" % response.text)         
+        else:
+            print("deletion failed: %s" % response.text)   
+        x += 1
+
+def deleteFiles():
+    if os.path.exists("deleteFiles.txt"):
+        os.remove("deleteFiles.txt")
+    else:
+        print("The file does not exist") 
+        
+    if os.path.exists("deleteFolders.txt"):
+        os.remove("deleteFolders.txt")
+    else:
+        print("The file does not exist")
+        
+    if os.path.exists("uploaded.txt"):
+        os.remove("uploaded.txt")
+    else:
+        print("The file does not exist")      
+    
 
 
 userhash = None
@@ -73,6 +170,33 @@ try:
 except Exception:
     print("catbox.py: no config file present")
 
+
+
+
 if __name__ == "__main__":
-    f = input("select file to upload")
-    print(upload(f))
+    while True:
+        inp = input ("Do you want to: A) Upload Images. B) Delete Images. [A/B]? : ").upper()
+        # check if d1a is equal to one of the strings, specified in the list
+        if inp in ['A', 'B']:
+            # if it was equal - break from the while loop
+            break
+    # process the input
+    if inp == "A": 
+        f = input("select the folder to upload ")
+        start = time.time()
+        upload(f)
+        end = time.time()
+        sec = end - start
+        td = timedelta(seconds=sec)
+        print()
+        print("Elapsed Time: " + str(td))        
+    elif inp == "B": 
+        start = time.time()
+        deletePics()
+        deleteAlbum()
+        deleteFiles()
+        end = time.time()
+        sec = end - start
+        td = timedelta(seconds=sec)  
+        print()
+        print("Elapsed Time: " + str(td))  
